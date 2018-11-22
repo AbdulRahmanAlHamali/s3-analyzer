9f64160096d5a05bc423ec2733a07ef03636ccbc
aalhamali <a.alhamali@saugo360.com>
2018-11-22 20:56:01 +0200

    implement cost calculation

--- src/s3_analyzer/global_analyzer.py
+++ src/s3_analyzer/global_analyzer.py
@@ -1,5 +1,7 @@
 import boto3
 import utils
+import math
+from ast import literal_eval
 
 
 class GlobalAnalyzer:
@@ -12,6 +14,9 @@ class GlobalAnalyzer:
         self.grouped_info = None
 
     def build_info(self):
+
+        pricing_info = self._prepare_pricing_info()
+
         s3 = boto3.resource('s3')
 
         for bucket in s3.buckets.all():
@@ -34,6 +39,8 @@ class GlobalAnalyzer:
 
             bucket_info['location'] = s3.meta.client.get_bucket_location(Bucket=bucket.name)['LocationConstraint']
 
+            bucket_info['cost'] = self._get_bucket_cost(bucket, pricing_info, bucket_info['location'])
+
             self.info_list.append(bucket_info)
 
         if self.filters is not None:
@@ -54,3 +61,73 @@ class GlobalAnalyzer:
         else:
             return utils.get_table_info(self.info_list)
 
+    def _get_bucket_cost(self, bucket, pricing_info, bucket_location):
+
+        total_by_storage_class = dict()
+
+        for object in bucket.objects.all():
+            storage_class = object.storage_class
+            if storage_class not in total_by_storage_class:
+                total_by_storage_class[storage_class] = 0
+            total_by_storage_class[storage_class] += object.size
+
+        cost = 0
+
+        for storage_class, size in total_by_storage_class.items():
+            class_pricing_info = pricing_info[storage_class][bucket_location]
+
+            for price_range in class_pricing_info:
+                if size >= price_range['begin_range'] and size < price_range['end_range']:
+                    cost += price_range['price'] * size / (1024 * 1024 * 1024)
+                    break
+
+        return cost
+
+    def _prepare_pricing_info(self):
+        # for some reason only the us-east-1 pricing endpoint is working
+        pricing = boto3.client('pricing', 'us-east-1')
+
+        volumeTypesResponse = pricing.get_attribute_values(
+            ServiceCode='AmazonS3',
+            AttributeName='volumeType'
+        )
+
+        volumeTypes = list(map(lambda x: x['Value'], volumeTypesResponse['AttributeValues']))
+        volumeTypes = list(filter(lambda x: x != 'Tags', volumeTypes))
+
+        result = dict()
+        for volumeType in volumeTypes:
+            price = pricing.get_products(
+                ServiceCode='AmazonS3',
+                Filters=[
+                    {
+                        'Type': 'TERM_MATCH',
+                        'Field': 'volumeType',
+                        'Value': volumeType
+                    }
+                ]
+            )
+
+            storage_class = utils.map_storage_class_name_to_code(volumeType)
+
+            result[storage_class] = dict()
+            for entry in price['PriceList']:
+                # because we receive it as a string
+                entry = literal_eval(entry)
+
+                location = entry['product']['attributes']['location']
+                location_code = utils.map_region_name_to_code(location)
+
+                options = next(iter(entry['terms']['OnDemand'].values()))['priceDimensions']
+
+                resulting_options = []
+                for _, option in options.items():
+                    resulting_options.append({
+                        'begin_range': int(option['beginRange']),
+                        'end_range': math.inf if option['endRange'] == 'Inf' else int(option['endRange']),
+                        'price': float(option['pricePerUnit']['USD'])
+                    })
+
+                result[storage_class][location_code] = resulting_options
+
+        return result
--- src/s3_analyzer/utils.py
+++ src/s3_analyzer/utils.py
@@ -55,3 +55,38 @@ def convert_size(value, target):
 
 def get_table_info(info):
     return tabulate(info, headers='keys', tablefmt="fancy_grid", numalign='left', stralign='left')
+
+def map_region_name_to_code(region_name):
+    regions = {
+        'US East (N. Virginia)': 'us-east-1',
+        'US East (Ohio)': 'us-east-2',
+        'US West (N. California)': 'us-west-1',
+        'US West (Oregon)': 'us-west-2',
+        'Canada (Central)': 'ca-central-1',
+        'EU (Frankfurt)': 'eu-central-1',
+        'EU (Ireland)': 'eu-west-1',
+        'EU (London)': 'eu-west-2',
+        'EU (Paris)': 'eu-west-3',
+        'Asia Pacific (Tokyo)': 'ap-northeast-1',
+        'Asia Pacific (Seoul)': 'ap-northeast-2',
+        'Asia Pacific (Osaka-Local)': 'ap-northeast-3',
+        'Asia Pacific (Singapore)': 'ap-southeast-1',
+        'Asia Pacific (Sydney)': 'ap-southeast-1',
+        'Asia Pacific (Mumbai)': 'ap-south-1',
+        'South America (Sao Paulo)': 'sa-east-1',
+        'AWS GovCloud (US-East)': 'us-gov-east-1',
+        'AWS GovCloud (US)': '	us-gov-west-1'
+    }
+
+    return regions[region_name]
+
+def map_storage_class_name_to_code(class_name):
+    classes = {
+        'One Zone - Infrequent Access': 'ONEZONE_IA',
+        'Standard - Infrequent Access': 'STANDARD_IA',
+        'Amazon Glacier': 'GLACIER',
+        'Standard': 'STANDARD',
+        'Reduced Redundancy': 'RRS'
+    }
+
+    return classes[class_name]
