ff3afe99016051dea5d0856da59be71b6a5e4cc1
aalhamali <a.alhamali@saugo360.com>
2018-11-22 21:19:09 +0200

    licence and installation

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(name='s3_analyzer',
+      version='0.1',
+      description='Usage analyzer for AWS S3',
+      url='https://github.com/AbdulRahmanAlHamali/s3_analyzer',
+      author='AbdulRahman AlHamali',
+      author_email='a.alhamali93@gmail.com',
+      license='MIT',
+      packages=['s3_analyzer'],
+      install_requires=[
+        'altgraph==0.16.1',
+        'boto3==1.9.48',
+        'botocore==1.12.48',
+        'docutils==0.14',
+        'future==0.17.1',
+        'jmespath==0.9.3',
+        'macholib==1.11',
+        'pefile==2018.8.8',
+        'python-dateutil==2.7.5',
+        's3transfer==0.1.13',
+        'six==1.11.0',
+        'tabulate==0.8.2',
+        'urllib3==1.24.1'
+      ],
+      zip_safe=False)
